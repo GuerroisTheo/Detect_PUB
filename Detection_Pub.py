22abefe428dc87283c810e69dcefc5537d754a97
GuerroisTheo <theo.guerrois@edu.esiee.fr>
2020-11-17 17:23:32 +0100

    Centrage et mise en place rectangle
    j'ai centralisé l'endroit des logos pour que IA ne detecte pas autre chose que le Logo

--- Detection_Pub.py
+++ Detection_Pub.py
@@ -1,14 +1,71 @@
 import cv2
 import numpy as np
-from PIL import ImageGrab
+from PIL import ImageGrab,ImageFilter
 import time 
 
+from ctypes import windll
+user32 = windll.user32
+user32.SetProcessDPIAware()
+def localisation(img):
 
-last_time = time.time()
+    image_rect = cv2.pyrDown(img)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    grad = cv2.morphologyEx(image_rect, cv2.MORPH_GRADIENT, kernel)
+
+    _, bw = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 1))
+    connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
+
+    contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    mask = np.zeros(bw.shape, dtype=np.uint8)
+
+    for idx in range(len(contours)):
+        x, y, w, h = cv2.boundingRect(contours[idx])
+        mask[y:y+h, x:x+w] = 0
+        cv2.drawContours(mask, contours, idx, (255, 255, 255), -1)
+        r = float(cv2.countNonZero(mask[y:y+h, x:x+w])) / (w * h)
+
+        if r > 0.45 and w > 8 and h > 8:
+            cv2.rectangle(image_rect, (x, y), (x+w-1, y+h-1), (0, 255, 0), 2)
+
+    return cv2.imshow('rects', image_rect)
+
+def ROI(img, sommet):
+    masque = np.zeros_like(img)
+    cv2.fillPoly(masque,sommet, 255)
+    img_masquee = cv2.bitwise_and(img,masque)
+    return img_masquee
+
+def process_image(image_ori):
+
+    processed_img = image_ori
+    L = processed_img.shape[1]
+    H = processed_img.shape[0]
+    HDL = int((L*5)/6)
+    HDH = int(H/8)
+
+    sommet = np.array([[L,H/25], [L,HDH], [HDL,HDH], [HDL,H/25]], np.int32)
+    new_img = ROI(processed_img,[sommet])
+
+    localisation(new_img)
+
+    
+    return new_img
+
+
+# last_time = time.time()
 while(True):
-    printscreen = np.array(ImageGrab.grab(bbox=(0,40, 800, 640)))
+    ecran = np.array(ImageGrab.grab().filter(ImageFilter.SHARPEN)) #bbox=(0,40, 900, 800)
 
-    cv2.imshow('window',cv2.cvtColor((printscreen), cv2.COLOR_BGR2RGB))
+    ecran_gris = cv2.cvtColor((ecran), cv2.COLOR_BGR2RGB)
+    ecran_gris = cv2.cvtColor((ecran), cv2.COLOR_BGR2GRAY)
+
+    new_ecran = process_image(ecran_gris)
+    # cv2.imshow('window1',cv2.cvtColor((new_ecran), cv2.COLOR_BGR2RGB))
+    # cv2.imshow('window2',cv2.cvtColor((ecran), cv2.COLOR_BGR2RGB))
 
 
     if cv2.waitKey(25)& 0xFF == ord('q'):
