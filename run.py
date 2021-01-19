5a432f34752243b3bfac46ea9dbb40aea138361e
GuerroisTheo <theo.guerrois@edu.esiee.fr>
2021-01-19 14:30:40 +0100

    queue pour les sauts humeurs

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import os
 from PIL import ImageGrab,ImageFilter
 import time
 import repeatedTime
+import collections
 from keras import utils, layers, optimizers, models
 from keras.preprocessing import image
 
@@ -15,6 +16,9 @@ user32 = windll.user32
 user32.SetProcessDPIAware()
 
 model = models.load_model('param.h5')
+
+g_queue = collections.deque()
+g_tempsatt = 5
 
 datagen = image.ImageDataGenerator(rescale=1./255, validation_split=0.2)
 
@@ -28,7 +32,7 @@ t2 = 0
 
 def init():
     global g_repscreen, g_bloqueur, g_klog
-    g_repscreen = repeatedTime.RepeatedTimer(1,timer)
+    g_repscreen = repeatedTime.RepeatedTimer(1,screen)
     g_klog = keylog.KeyLogger()
     startAll()
 
@@ -45,49 +49,47 @@ def stopAll():
 
 
 def screen():
-    screen = ImageGrab.grab(bbox=(1594, 41, 1902 , 137))
-    #image = cv2.cvtColor((screen), cv2.COLOR_BGR2RGB)
-
-    FILES_DIR = 'C:/Users/Theo/Documents/GitHub/Detect_PUB/puber'
-    SAVE_PATH = 'C:/Users/Theo/Documents/GitHub/Detect_PUB/puber'
-    #SAVE_PATH = os.path.expanduser("~")    #It is cross-platform
-    LOGFILE_NAME = "puber.png"
-
-    LOGFILE_PATH = os.path.join(SAVE_PATH, FILES_DIR, LOGFILE_NAME)
-    screen.save(LOGFILE_PATH)
-
-    #image = cv2.resize(image, (40,40))
-    #return image
-
-    #test = cv2.imread('C:/Users/TLG/Desktop/IAImage/ProjetPubE4/Detect_PUB//puber/puber.png', cv2.IMREAD_UNCHANGED)
-    #print(test.shape)
-    #test.shape = (1,96,308,3)
-    #print(test.shape)
-
-    test = datagen.flow_from_directory("./puber", class_mode=None, target_size=(40,40), batch_size=1)
-    
-    prediction = model.predict(test[0])
-    labels = np.argmax(prediction, axis=1)
-    print(labels)
-    #print(CATEGORIES[int(prediction[0][0])])
-    return CATEGORIES[int(labels)]
-
-def timer():
-    global g_repscreen, g_bloqueur, tempsPub, t1, t2, g_klog
-    
+    global g_tempsatt
     if (g_klog.a_stopMain):
-        cat = screen()
+        screen = ImageGrab.grab(bbox=(1594, 41, 1902 , 137))
+        FILES_DIR = 'C:/Users/Theo/Documents/GitHub/Detect_PUB/puber'
+        SAVE_PATH = 'C:/Users/Theo/Documents/GitHub/Detect_PUB/puber'
+        LOGFILE_NAME = "puber.png"
 
-        if cat == "PUB" and g_bloqueur == 1:
-            t1 = time.time()
-            g_bloqueur = 0
-        if cat != "PUB" and g_bloqueur == 0:
-            t2 = time.time()
-            tempsPub.append(t2-t1)
-            print(t2-t1)
-            g_bloqueur = 1
+        LOGFILE_PATH = os.path.join(SAVE_PATH, FILES_DIR, LOGFILE_NAME)
+        screen.save(LOGFILE_PATH)
+        test = datagen.flow_from_directory("./puber", class_mode=None, target_size=(40,40), batch_size=1)
+        
+        prediction = model.predict(test[0])
+        labels = np.argmax(prediction, axis=1)
+        
+        g_queue.append(labels)
+
+        if len(g_queue)>g_tempsatt : 
+                timer(g_queue)
+                taillemaxqueue(g_tempsatt,g_queue)
     else:
         stopAll()
+
+
+def taillemaxqueue(max,queue):
+    if len(queue)>max:
+        queue.popleft()
+        taillemaxqueue(max,queue)
+
+def timer(g_queue):
+    global g_repscreen, g_bloqueur, tempsPub, t1, t2, g_klog, g_tempsatt
+
+    labels = list(collections.deque(g_queue))
+    
+    if CATEGORIES[int(labels[-1])] != "PUB" and g_bloqueur == 1:
+        t1 = time.time()
+        g_bloqueur = 0
+    if CATEGORIES[int(labels[-1])] == "PUB" and g_bloqueur == 0:
+        t2 = time.time()
+        tempsPub.append(t2-t1)
+        print(t2-t1)
+        g_bloqueur = 1
 
 
 if __name__ == "__main__":
